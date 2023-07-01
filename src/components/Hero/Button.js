74f85c0a91f26ff825930f2a2023a948a257deb7
Lakhwinderr <lakhwinder.sidhuu20@gmail.com>
2023-07-02 05:13:31 +0530

    modified:   src/components/Hero/Button.js

--- src/components/Hero/Button.js
+++ src/components/Hero/Button.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 const Button = () => {
     const navigate = useNavigate();
     const routeChange = () => {
-        navigate('"/little-lemon/reservations')
+        navigate("/little-lemon/reservations")
     }
     return <button className='mainButton' onClick = {routeChange}>Reserve a Table</button>
 }
